d08c75d7e3d2396ee4f5a0482ca1ea6acd9586bf
ziadhema11 <111283628+ziadhema11@users.noreply.github.com>
2022-09-07 23:43:39 +0200

    Update keepAlive.js

--- keepAlive.js
+++ keepAlive.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const server = express();
  
-server.all('/', (req, res) => {
-  res.send(OK)
-})
+// server.all('/', (req, res) => {
+//   res.send(OK)
+// })
  
 function keepAlive() {
   server.listen(3000, () => { console.log("" + Date.now()) });
